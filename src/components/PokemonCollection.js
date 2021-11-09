77bfcc0da05c4ecab618e53f185ef76cdbb33de5
Andrew Busel <buselax@gmail.com>
2021-11-09 10:58:07 -0500

    completed assignment

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,10 +2,12 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({displayedPokemon, input}) {
   return (
     <Card.Group itemsPerRow={6}>
-      <h1>Hello From Pokemon Collection</h1>
+      {displayedPokemon.filter((pokemon)=>pokemon.name.includes(input)).map((pokemon)=>{
+        return <PokemonCard pokemon={pokemon} key={pokemon.id} />
+      })}
     </Card.Group>
   );
 }
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [displayedPokemon, setDisplayedPokemon] = useState([])
+  const [input, setInput] = useState('')
+  useEffect(()=>{
+    fetch('http://localhost:3001/pokemon')
+    .then(res=> res.json())
+    .then(data=> setDisplayedPokemon(data))
+  },[])
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm setDisplayedPokemon={setDisplayedPokemon} displayedPokemon={displayedPokemon}/>
       <br />
-      <Search />
+      <Search setDisplayedPokemon={setDisplayedPokemon} displayedPokemon={displayedPokemon} input= {input} setInput={setInput}/>
       <br />
-      <PokemonCollection />
+      <PokemonCollection displayedPokemon={displayedPokemon} input={input}/>
     </Container>
   );
 }
